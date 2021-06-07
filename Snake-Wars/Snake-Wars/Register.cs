93f50c2cb453421ae4418069f7b88b654cc6c604
feiye17 <82388571+feiye17@users.noreply.github.com>
2021-06-07 18:38:50 +0800

    Add files via upload

--- Snake-Wars/Snake-Wars/Register.cs
+++ Snake-Wars/Snake-Wars/Register.cs
@@ -17,5 +17,34 @@ namespace Snake_Wars
             InitializeComponent();
         }
 
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+                //复选框被勾选，明文显示
+                passwordBox1.PasswordChar = new char();
+            else
+                //复选框被取消勾选，密文显示
+                passwordBox1.PasswordChar = '*';
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked)
+                //复选框被勾选，明文显示
+                passwordBox2.PasswordChar = new char();
+            else
+                //复选框被取消勾选，密文显示
+                passwordBox2.PasswordChar = '*';
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
