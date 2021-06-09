0d2be19b0e361a15c8884133d1fecd5aacb84e21
ElioEivu <82389512+ElioEivu@users.noreply.github.com>
2021-06-09 11:02:50 +0800

    Add files via upload

--- Snake-Wars/Snake-Wars/Login.Designer.cs
+++ Snake-Wars/Snake-Wars/Login.Designer.cs
@@ -30,22 +30,28 @@ namespace Snake_Wars
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
             this.label2 = new System.Windows.Forms.Label();
             this.passwordBox = new System.Windows.Forms.TextBox();
             this.Register = new System.Windows.Forms.Label();
             this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("宋体", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label2.Location = new System.Drawing.Point(10, 85);
+            this.label2.Font = new System.Drawing.Font("宋体", 10.89076F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(10, 129);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(66, 19);
             this.label2.TabIndex = 1;
-            this.label2.Text = "账号：";
+            this.label2.Text = "密码：";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // passwordBox
             // 
@@ -55,6 +61,7 @@ namespace Snake_Wars
             this.passwordBox.PasswordChar = '*';
             this.passwordBox.Size = new System.Drawing.Size(182, 28);
             this.passwordBox.TabIndex = 4;
+            this.passwordBox.TextChanged += new System.EventHandler(this.passwordBox_TextChanged);
             // 
             // Register
             // 
@@ -62,7 +69,7 @@ namespace Snake_Wars
             this.Register.Font = new System.Drawing.Font("宋体", 10.8F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.Register.Location = new System.Drawing.Point(285, 85);
             this.Register.Name = "Register";
-            this.Register.Size = new System.Drawing.Size(85, 19);
+            this.Register.Size = new System.Drawing.Size(80, 18);
             this.Register.TabIndex = 5;
             this.Register.Text = "注册账号";
             this.Register.Click += new System.EventHandler(this.Register_Click);
@@ -70,36 +77,87 @@ namespace Snake_Wars
             // button3
             // 
             this.button3.AutoSize = true;
-            this.button3.Font = new System.Drawing.Font("宋体", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.button3.Location = new System.Drawing.Point(44, 205);
+            this.button3.Font = new System.Drawing.Font("宋体", 13.91597F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button3.Location = new System.Drawing.Point(44, 182);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(89, 30);
+            this.button3.Size = new System.Drawing.Size(115, 53);
             this.button3.TabIndex = 11;
-            this.button3.Text = "button1";
+            this.button3.Text = "登录";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 10.89076F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(10, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 19);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "账号：";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("宋体", 10.89076F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.checkBox1.Location = new System.Drawing.Point(270, 129);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(101, 19);
+            this.checkBox1.Size = new System.Drawing.Size(104, 23);
             this.checkBox1.TabIndex = 12;
-            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.Text = "显示密码";
             this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("宋体", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.Location = new System.Drawing.Point(82, 81);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(182, 28);
+            this.textBox1.TabIndex = 14;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("宋体", 10.89076F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(12, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(180, 19);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "请输入您的账号密码";
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.Font = new System.Drawing.Font("宋体", 13.91597F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(222, 182);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 53);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "退出";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(482, 653);
+            this.ClientSize = new System.Drawing.Size(382, 254);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.Register);
             this.Controls.Add(this.passwordBox);
             this.Controls.Add(this.label2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Login";
-            this.Text = "Login";
+            this.Text = "登录";
+            this.Load += new System.EventHandler(this.Login_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -110,6 +168,10 @@ namespace Snake_Wars
         private System.Windows.Forms.TextBox passwordBox;
         private System.Windows.Forms.Label Register;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
     }
 }
